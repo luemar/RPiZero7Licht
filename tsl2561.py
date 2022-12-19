0569fa6d3c324d36534c3bd227f8eeb3c6da4ae2
marcus luetolf <marcus.luetolf@bluewin.ch>
2022-12-19 17:00:04 +0100

    Update tsl2561.py

--- tsl2561.py
+++ tsl2561.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+import adafruit_tsl2561
 import time
 import board
 import busio
-import adafruit_tsl2561
-from os import system
-import smbus
 import sys
-from gpiozero import LED
 import random
 import logging
+from gpiozero import LED
+from os import system
 
-t = time.ctime()
-logging.basicConfig(filename='/home/pi/mylog1.log', filemode='w',level=logging.DEBUG, format='%(message)s:%(asctime)s')
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_tsl2561.TSL2561(i2c)
+treshold = 62
+logging.basicConfig(filename='/home/pi/mylog1.log',\
+                    filemode='w',level=logging.DEBUG,\
+                    format='%(message)s:%(asctime)s')
 
 spot_main = LED(13)
 spot_aux = LED(19)
@@ -26,127 +29,128 @@ def month(time):
     tm1 = time.split()
     return  tm1[1]
 
-tx = time_in_seconds(time.ctime())
-print('tsl2561.py started at: ',time.ctime(), 'equals:', tx, 'seconds')
+def sensor_read():
+    brightness = sensor.lux
+    return brightness
+
+sec_at_start = time_in_seconds(time.ctime())
 time.sleep(0.5)
+tsl2561_start_time = time.ctime()
+print('tsl2561.py started at: ',\
+       tsl2561_start_time,'equals:', sec_at_start)
 logging.info('tsl2561.py started ')
 
-def sensor_read ():
-
-    i2c = busio.I2C(board.SCL, board.SDA) # Create the I2C bus
-    tsl = adafruit_tsl2561.TSL2561(i2c)  # Create the TSL2561 instance, passing>
-
-    #tsl.enabled = True  # Enable the light sensor
+if sec_at_start >= 71940:
     time.sleep(1)
-    tsl.gain = 0  # Set gain 0=1x, 1=16x
-    tsl.integration_time = 1 # Set integration time (0=13.7ms, 1=101ms, 2=402ms>
+    spot_gallery.on()
+    print('direkt start, gallery_on')
+    logging.info('direk start, gallery_on')
+    time.sleep(85500-sec_at_start)
+    spot_gallery.off()
+    print('gallery_off nach direkt start')
+    logging.info('gallery_off nach direkt start')
+    sys.exit()
 
-    # Get raw (luminosity) readings individually
-    broadband = tsl.broadband
-    infrared = tsl.infrared
+else:
+    while True:
+        print('Brightness =',sensor_read())
+        running_time = time.ctime()
+	running_sec = time_in_seconds(running_time)
+        print('current_running_time: ',running_time)
+        logging.info('sensor_read() started')
+        time.sleep(20)
 
-    # Get raw (luminosity) readings using tuple unpacking
-    # broadband, infrared = tsl.luminosity
-
-    # Get computed lux value (tsl.lux can return None or a float)
-    lux = tsl.lux
-    if lux is None:
-        lux = 0
-    return lux
-
-#tsl.enabled = False # Disble the light sensor (to save power)
-
-while True:
-    time.sleep(120)
-    print('Lux =',sensor_read(),'at:',time.ctime(),'equals:',time_in_seconds(ti>
-    logging.info('sensor_read() started')
-
-    if sensor_read() < 120:
-        time.sleep(5)
-        print('below 120')
-        logging.info('lux below 120')
-
-        if month(time.ctime()) == 'Feb' or month(time.ctime()) == 'Mar' or mont>
-            spot_main.on()
-            logging.inf0('main on')
-            time.sleep(69600 - tx)
-            spot_gallery.on() #19:20
-            logging.inf0('gallery on')
-            time.sleep(4200 + random.randint(60, 180))
-            spot_aux.on() #20:31 - 20:33
-            logging.inf0('aux on')
-            time.sleep(600)
-            spot_main.off() #20:41 - 20:43
-            logging.inf0('main off')
-            time.sleep(3600 + random.randint(60, 180))
-            spot_aux.off() #21:42 - 21:46
-            logging.inf0('aux off')
-            time.sleep(7200)
-            spot_gallery.off() #23:42 - 23:46
-            logging.inf0('gallery off')
-
-
-        elif month(time.ctime()) == 'May' or month(time.ctime()) == 'Jun' or mo>
+        if  sensor_read() < treshold:
             time.sleep(1)
-            spot_gallery.on()
-            logging.info('gallery on')
-            time.sleep(78000 - tx)
-            spot_main.on() #21:40
-            logging.info('main on')
-            time.sleep(900 + random.randint(60, 120))
-            spot_aux.on() #21:56 - 21:57
-            logging.info('aux on')
-            time.sleep(1200 + random.randint(60, 180))
-            spot_main.off() #22:17 - 22:20
-            logging.info('main off')
-            time.sleep(3600)
-            spot_aux.off() #23:17 - 23:20
-            logging.info('aux off')
-            time.sleep(1800)
-            spot_gallery.off() #23:39 - 23:54
-            logging.info('gallery off')
+            below_treshold_time = time.ctime()
+            print('brightness below treshold at:',\
+                  below_treshold_time)
+            logging.info('brightness below treshold')
+
+
+            if month(time.ctime()) == 'Feb' or month(time.ctime())\
+               == 'Mar' or month(time.ctime()) == 'Apr':
+                spot_main.on()
+                logging.info('main on')
+                time.sleep(71940 - sec_at_start)
+                spot_gallery.on() #19:59
+                logging.info('gallery on')
+                time.sleep(6540 + random.randint(60, 180))
+                spot_aux.on() #21:11 - 21:15
+                logging.info('aux on')
+                time.sleep(600)
+                spot_main.off() #21:21 - 21:25
+                logging.info('main off')
+                time.sleep(1260 + random.randint(60, 180))
+                spot_aux.off() #21:42 - 21:46
+                logging.info('aux off')
+                time.sleep(7200)
+                spot_gallery.off() #23:42 - 23:46
+                logging.info('gallery off')
+
+
+            elif month(time.ctime()) == 'May' or month(time.ctime())\
+                 == 'Jun' or month(time.ctime())== 'July':
+                time.sleep(1)
+                spot_gallery.on()
+                logging.info('gallery on')
+                time.sleep(78000 - sec_at_start)
+                spot_main.on() #21:40
+                logging.info('main on')
+                time.sleep(900 + random.randint(60, 120))
+                spot_aux.on() #21:56 - 21:57
+                logging.info('aux on')
+                time.sleep(1200 + random.randint(60, 180))
+                spot_main.off() #22:17 - 22:20
+                logging.info('main off')
+                time.sleep(3600)
+                spot_aux.off() #23:17 - 23:20
+                logging.info('aux off')
+                time.sleep(1800)
+                spot_gallery.off() #23:39 - 23:54
+                logging.info('gallery off')
 			
-		 elif month(time.ctime()) == 'Aug'or month(time.ctime()) == 'Sep'or mont>
-            time.sleep(0.5)
-            spot_main.on()
-            logging.info('spot_main.on')
-            time.sleep(72600 - tx + (random.randint(120, 300)))
-            spot_aux.on() #20:10+2Min - 20:10+5Min.
-            logging.info('spot_aux.on')
-            time.sleep(4500)
-            spot_gallery.on() #21:27 - 21:30
-            logging.info('spot_gallery.on')
-            time.sleep(random.randint(300, 600))
-            spot_main.off()  #21:32 - 21:40
-            logging.info('spot_main.off')
-            time.sleep(3600)
-            spot_aux.off() #22:32 -22:40
-            logging.info('spot_aux.off')
-            time.sleep(2280 + random.randint(300, 600))
-            spot_gallery.off() #23:10 +3Min. = 23:13 - 23:18 +10Min. = 23:28
-            logging.info('spot_gallery.off')
+	    elif month(time.ctime()) == 'Aug'or month(time.ctime())\
+                 == 'Sep' or month(time.ctime())== 'Oct':
+                time.sleep(0.5)
+                spot_main.on()
+                logging.info('spot_main.on')
+                time.sleep(72600 - sec_at_start + (random.randint(120, 300)))
+                spot_aux.on() #20:10+2Min - 20:10+5Min.
+                logging.info('spot_aux.on')
+                time.sleep(4500)
+                spot_gallery.on() #21:27 - 21:30
+                logging.info('spot_gallery.on')
+                time.sleep(random.randint(300, 600))
+                spot_main.off()  #21:32 - 21:40
+                logging.info('spot_main.off')
+                time.sleep(3600)
+                spot_aux.off() #22:32 -22:40
+                logging.info('spot_aux.off')
+                time.sleep(2280 + random.randint(300, 600))
+                spot_gallery.off() #23:10 +3Min. = 23:13 - 23:18 +10Min. = 2>
+                logging.info('spot_gallery.off')
 
-
-        elif month(time.ctime()) == 'Nov'or month(time.ctime()) == 'Dec'or mont>
-            time.sleep(0.5)
-            spot_main.on()
-            logging.info('spot_main.on')
-            time.sleep((67200 - tx) + (random.randint(180, 300)))
-            spot_aux.on() #18:43 - 18:45
-            logging.info('spot_aux.on')
-            time.sleep(1500)
-            spot_gallery.on() #19:08 - 19:10
-            logging.info('spot_gallery.on')
-            time.sleep(2700)
-            spot_main.off()  #19:53 - 19:55
-            logging.info('spot_main.off')
-            time.sleep(4500)
-            spot_aux.off() #21:08 -21:10
-            logging.info('spot_aux.off')
-            time.sleep(9000)
-            spot_gallery.off() #23:40 - 23:43
-            logging.info('spot_gallery.off')
+            elif month(time.ctime()) == 'Nov'or month(time.ctime())\
+                 == 'Dec' or month(time.ctime())== 'Jan':
+                time.sleep(0.5)
+                spot_main.on()
+                logging.info('spot_main.on')
+                time.sleep((67200 - sec_at_start) + (random.randint(180, 300)))
+                spot_aux.on() #18:43 - 18:45
+                logging.info('spot_aux.on')
+                time.sleep(1500)
+                spot_gallery.on() #19:08 - 19:10
+                logging.info('spot_gallery.on')
+                time.sleep(2700)
+                spot_main.off()  #19:53 - 19:55
+                logging.info('spot_main.off')
+                time.sleep(4500)
+                spot_aux.off() #21:08 -21:10
+                logging.info('spot_aux.off')
+                time.sleep(9000)
+                spot_gallery.off() #23:40 - 23:43
+                logging.info('spot_gallery.off')
+            logging.info('program terminated')
         break
 sys.exit()
-
-
